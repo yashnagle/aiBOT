ae029f32aec3255108edb2359a78d495d39b2744
yashnagle <yash.nagle1705@gmail.com>
2024-07-11 21:15:01 -0400

    updated query page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Header">
+        <header className="title">
+          <h1>aiBot</h1>
+          <h2>Ask me anything!</h2>
+        </header>
+      </div>
+
+      <div className="TextEntry">
+        <input type="query" placeholder="Enter Text Here" />
+      </div>
     </div>
   );
 }
