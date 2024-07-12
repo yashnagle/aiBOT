8e846e0ef8f4e74c93826f7cc1f4dc0c5ae3784e
yashnagle <yash.nagle1705@gmail.com>
2024-07-11 23:01:52 -0400

    added logo and query box

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import logo from "./logo.svg";
 import "./App.css";
+import robot_icon from "./robot_icon.png";
 
 function App() {
   return (
     <div className="App">
       <div className="Header">
         <header className="title">
-          <h1>aiBot</h1>
-          <h2>Ask me anything!</h2>
+          <div className="icon-title">
+            <img className="icon" src={robot_icon} alt="aiBot logo" />
+            <h1 className="icon_title">aiBot</h1>
+          </div>
+          <h2 className="subtitle">Ask me anything!</h2>
         </header>
       </div>
 
       <div className="TextEntry">
-        <input type="query" placeholder="Enter Text Here" />
+        {/* <input type="query" placeholder="Enter Text Here" /> */}
+        <textarea class="query-input" placeholder="Enter query here"></textarea>
       </div>
     </div>
   );
