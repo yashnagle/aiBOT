c59508bb01aa4021159cc1e56e5eeb66f7f4cf14
yashnagle <yash.nagle1705@gmail.com>
2024-07-13 00:20:41 -0400

    improved textbox

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import React, { Component, useState } from "react";
 
 function App() {
   const [val, setVal] = useState("");
+  const [query, setQuery] = useState("");
+
   const click1 = () => {
     if (val === "") {
       console.log("No input!");
@@ -12,12 +14,21 @@ function App() {
       console.log("Query: ", val);
       console.log("Making API call!");
     }
+
+    setQuery("");
     // make the API call to send the query
   };
 
   const change1 = (event) => {
     setVal(event.target.value);
+    setQuery(event.target.value);
   };
+
+  function uploadFile(event) {
+    const file = event.target.files[0];
+    console.log(file);
+    // You can now handle the file upload logic here
+  }
 
   return (
     <div className="App">
@@ -33,12 +44,14 @@ function App() {
       <div className="TextEntry">
         <textarea
           className="query-input"
+          value={query}
           onChange={change1}
           placeholder="Enter query here"
         ></textarea>
         <button className="submit-button" onClick={click1}>
           Submit
         </button>
+        <input type="file" className="file-upload" onChange={uploadFile} />
       </div>
     </div>
   );
